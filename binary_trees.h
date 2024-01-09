3ce14a24e40d3c67bbf11b5797978aa81ae240a2
xov121 <x.o.v121@gmail.com>
2024-01-09 18:25:28 -0400

    Added prototype for printing function

--- binary_trees.h
+++ binary_trees.h
@@ -46,4 +46,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+
+/* Printing function */
+void binary_tree_print(const binary_tree_t *);
+
 #endif /* BINARY_TREES_H */
